290cf2bd8417ec26ffa71049122a4145127d77e0
Bryan Mehall <bryanmehall@gmail.com>
2018-07-21 10:37:15 -0600

    add integration test framework

--- dev/integration.test.js
+++ dev/integration.test.js
@@ -1,15 +1,79 @@
+import { lynxParser } from './lynxParser'
+import { compile } from './ducks/object/selectors'
+
 var fs = require('fs');
 
-fs.readFile( __dirname + '/../courses/experimental/lynx/core.lynx', function (err, data) {
-  if (err) {
-    throw err;
-  }
-    describe('Integration', () => {
-      it('test integration test', () => {
-        expect(true).toBe(true);
-      });
-    });
-  console.log(data.toString());
+const getAST = (testName, callback) =>{
+
+}
+const runTest = (testName, objects, done) => {
+    const state = {sim:{object:objects}}
+    const {renderMonad, functionTable} = compile(state)
+    let pass = false
+    Object.entries(functionTable).forEach((func) => {
+        const st = func.toString()
+        console.log("#####", st)
+        if (st.includes('20')&&st.includes('test') && !st.includes('undefined')){
+            pass = true
+        }
+    })
+    if (pass){
+        done()
+    } else {
+        done.fail('conditions not met')
+    }
+}
+
+const loadAndRunTest = (testName, done) => {
+    const corePath = __dirname + '/../courses/experimental/lynx/core.lynx'
+    const path = `${__dirname}/../courses/experimental/lynx/${testName}.lynx`
+    let filesRead = 0
+    let firstData
+    fs.readFile(corePath, 'utf8', (err, data) => {
+        if (err) {
+            done.fail('could not read core')
+        } else {
+            filesRead+=1
+            checkComplete(data)
+            firstData = data
+        }
+    })
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            done.fail('could not read filename')
+        } else {
+            filesRead+=1
+            checkComplete(data)
+            firstData = data
+        }
+    })
+    const checkComplete = (data) =>{
+        if (filesRead === 2){
+            const dataSources = [firstData, data]
+            const objectSources = dataSources.map((data) => (lynxParser(data)))
+            const objects = Object.assign({}, objectSources[0], objectSources[1])
+            runTest(testName, objects, done)
+        }
+    }
+}
+
+const testNames = [
+    'simple-get',
+    'multiple-get',
+    //'get-end-chain',
+    //'non-local-root',
+    //'parent-path'
+
+]
+const testString = testNames.map((name) => (
+        `it('${name}', (done) => {`+
+        `loadAndRunTest('${name}', done)`+
+        `})`
+    )).join('\n')
+
+
+
+describe('simple-get', () => {
+    eval(testString)
+    //terrible hack because jest doesn't have programatic test generation
 })
-
-
